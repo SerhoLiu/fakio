6f1ee103dfab0db31cd39a2751d2ae722cbeb331
Serho Liu <serholiu@gmail.com>
2013-05-04 15:11:12 +0800

    filename change, event to fevent

---
+++ fevent.h
@@ -0,0 +1,46 @@
+#ifndef _EVENT_H_
+#define _EVENT_H_
+
+#define EV_NONE 0
+#define EV_RDABLE 1
+#define EV_WRABLE 2
+
+#define EV_WAIT 1
+
+struct event_loop;
+
+typedef void ev_callback(struct event_loop *loop, int fd, int mask, void *evdata);
+
+typedef struct ev_event {
+    int mask;
+    ev_callback *ev_read;  
+    ev_callback *ev_write;
+    void *evdata;       
+} ev_event;
+
+typedef struct ev_fired {
+    int fd;
+    int mask;   
+} ev_fired;
+
+typedef struct event_loop {
+    int maxfd;
+    int setsize;
+    ev_event *events;
+    ev_fired *fireds;
+    int stop;
+    void *apidata;
+} event_loop;
+
+event_loop *create_event_loop(int setsize);
+void delete_event_loop(event_loop *loop);
+int process_events(event_loop *loop, int flags);
+void start_event_loop(event_loop *loop);
+void stop_event_loop(event_loop *loop);
+
+int create_event(event_loop *loop, int fd, int mask,
+                 ev_callback *cb, void *evdata);
+void delete_event(event_loop *loop, int fd, int mask);
+int get_event_mask(event_loop *loop, int fd);
+
+#endif
