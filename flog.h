7c63870c129aad56c7ef68ceb36cac4d84d7c0a5
Serho Liu <serholiu@gmail.com>
2013-05-04 15:14:17 +0800

    加入 NDEBUG 选项

--- flog.h
+++ flog.h
@@ -8,12 +8,20 @@
 #define STR(x) #x
 #define TOSTR(x) STR(x)
 
-#define LOG_INFO(format, ...) do {\
+#ifdef NDEBUG
+  
+  #define LOG_INFO(format, ...) (void)0
+
+#else
+
+  #define LOG_INFO(format, ...) do {\
                           time_t now = time(NULL);\
                           char timestr[20];\
                           strftime(timestr, 20, "%F %T", localtime(&now));\
                           fprintf(stderr, "\e[32m[%s INFO] \e[0m" format "\n", timestr, ##__VA_ARGS__);}\
                           while(0)
+
+#endif
 
 #define LOG_WARN(format, ...) do {\
                           time_t now = time(NULL);\
@@ -22,11 +30,13 @@
                           fprintf(stderr, "\e[35m[%s WARN] \e[0m" format " %s:%s\n", timestr, ##__VA_ARGS__, __FILE__, TOSTR(__LINE__));}\
                           while(0)
 
+
 #define LOG_ERROR(format, ...) do {\
                           time_t now = time(NULL);\
                           char timestr[20];\
                           strftime(timestr, 20, "%F %T", localtime(&now));\
                           fprintf(stderr, "\e[31m[%s ERROR] \e[0m" format " %s:%s\n", timestr, ##__VA_ARGS__, __FILE__, TOSTR(__LINE__));exit(1);}\
                           while(0)
+
 
 #endif
