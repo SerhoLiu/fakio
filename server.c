650fea9854a67fd9abe9e3f7a2237a99fa1a4989
Serho Liu <serholiu@gmail.com>
2013-05-04 16:34:24 +0800

    小修改

--- server.c
+++ server.c
@@ -1,5 +1,5 @@
 #include "flog.h"
-#include "event.h"
+#include "fevent.h"
 #include "fnet.h"
 #include "fcrypt.h"
 #include <sys/socket.h>
@@ -11,123 +11,90 @@
 #include <unistd.h>
 #include <signal.h>
 
-#define BUFSIZE 1440
-
-int malloc_count = 0;
-
-typedef struct context {
-    int client_fd;
-    int remote_fd;
-    char csend[BUFSIZE];
-    char crecv[BUFSIZE];
-    int sendlen;
-    int recvlen;
-    struct event_loop *loop;
-} context;
-
-static void close_and_free_client(context *c)
-{
-    delete_event(c->loop, c->client_fd, EV_WRABLE);
-    delete_event(c->loop, c->client_fd, EV_RDABLE);
-    close(c->client_fd);
-    int x = c->client_fd;
-    c->client_fd = 0;
-    if (c->client_fd || c->remote_fd) {
-        return;
-    }
-    free(c);
-    LOG_WARN("%d I'm Free form client......................", x);    
-}
-
-static void close_and_free_remote(context *c)
-{
-    delete_event(c->loop, c->remote_fd, EV_WRABLE);
-    delete_event(c->loop, c->remote_fd, EV_RDABLE);
-    close(c->remote_fd);
-    int x = c->remote_fd;
-    c->remote_fd = 0;
-    if (c->client_fd || c->remote_fd) {
-        return;
-    }
-    free(c);
-    LOG_WARN("%d I'm Free form remote......................", x);     
-}
-
-
-
-void client_read_cb(struct event_loop *loop, int fd, int mask, void *evdata);
-void server_remote_reply_cb(struct event_loop *loop, int fd, int mask, void *evdata);
-void remote_write_cb(struct event_loop *loop, int fd, int mask, void *evdata);
-void remote_read_cb(struct event_loop *loop, int fd, int mask, void *evdata);
+static void client_readable_cb(struct event_loop *loop, int fd, int mask, void *evdata);
+static void server_remote_reply_cb(struct event_loop *loop, int fd, int mask, void *evdata);
+static void remote_writable_cb(struct event_loop *loop, int fd, int mask, void *evdata);
+static void remote_readable_cb(struct event_loop *loop, int fd, int mask, void *evdata);
 
 /* client 可写 */
-void client_write_cb(struct event_loop *loop, int fd, int mask, void *evdata)
+void client_writable_cb(struct event_loop *loop, int fd, int mask, void *evdata)
 {
+
     context *c = (context *)evdata;
     
-    if (c->remote_fd == 0) {
+    if (c->remote_fd == 0 || c->recvlen == 0) {
         close_and_free_client(c);
         return;
     }
 
     while (1) {
-        int rc = send(fd, c->crecv, c->recvlen, 0);
+        int rc = send(fd, c->crecv + c->rnow, c->recvlen, 0);
         if (rc < 0) {
             if (errno != EAGAIN) {
-                LOG_WARN("send() for client %d failed: %s", fd, strerror(errno));
+                LOG_WARN("send() to client %d failed: %s", fd, strerror(errno));
                 close_and_free_client(c);
                 close_and_free_remote(c);
-                break;
+                return;
             }
             break; 
         }
         if (rc >= 0) {
+            /* 当发送 rc 字节的数据后，如果系统发送缓冲区满，则会产生 EAGAIN 错误，
+             * 此时若 rc < c->recvlen，则再次发送时，会丢失 recv buffer 中的
+             * c->recvlen - rc 中的数据，因此应该将其移到 recv buffer 前面
+             */ 
             c->recvlen -= rc;
+
+            /* OK，数据一次性发送完毕，不需要特殊处理 */
             if (c->recvlen <= 0) {
-                //LOG_INFO("Send to client %d OK!!!!!!", c->client_fd);
+                c->rnow = 0;
                 delete_event(loop, fd, EV_WRABLE);
-                create_event(loop, c->client_fd, EV_RDABLE, &client_read_cb, (void *)c); 
-                create_event(loop, c->remote_fd, EV_RDABLE, &remote_read_cb, (void *)c);
-                break;
+                create_event(loop, c->client_fd, EV_RDABLE, &client_readable_cb, c); 
+                create_event(loop, c->remote_fd, EV_RDABLE, &remote_readable_cb, c);
+                return;
+            } else {
+                c->rnow += rc;
             }
-            continue;
         }
     }  
 }
 
 
-void client_read_cb(struct event_loop *loop, int fd, int mask, void *evdata)
+void client_readable_cb(struct event_loop *loop, int fd, int mask, void *evdata)
 {
     context *c = (context *)evdata;
-    
+
     while (1) {
         int rc = recv(fd, c->csend, BUFSIZE, 0);
         if (rc < 0) {
             if (errno != EAGAIN) {
-                LOG_WARN("recv() failed: %s", strerror(errno));
+                LOG_WARN("recv() from client %d failed: %s", fd, strerror(errno));
                 close_and_free_client(c);
                 close_and_free_remote(c);
                 return;
             }
-            
             delete_event(loop, fd, EV_RDABLE);
             break; 
         }
         if (rc == 0) {
-            LOG_WARN("client %d connection closed\n", fd);
+            LOG_WARN("client %d connection closed", fd);
             close_and_free_client(c);
-            LOG_WARN("context client %d and remote %d ", c->client_fd, c->remote_fd);
             break;
         }
-        c->sendlen += rc;
         
-        if (c->sendlen > BUFSIZE - 1) {
-            delete_event(loop, fd, EV_RDABLE);
-            break;    
-        }
+        c->sendlen += rc;
+        /* 通常情况下 rc 和 BUFSIZE 差不多，不过有时候 rc 比较小，如果 EAGAIN 没有
+         * 发生，那么连续接收有可能造成 csend buffer 溢出，所以这里就有一个问题：怎样
+         * 尽可能的多接收数据后再进行发送
+         * 目前是不管多少，收到即发
+         */
+        //encrypt(c->csend, rc);
+        delete_event(loop, fd, EV_RDABLE);
+        break;    
     }
-    create_event(loop, c->remote_fd, EV_WRABLE, &remote_write_cb, (void *)c);
+    create_event(loop, c->remote_fd, EV_WRABLE, &remote_writable_cb, c);
 }
+
 
 void server_accept_cb(struct event_loop *loop, int fd, int mask, void *evdata)
 {
@@ -136,7 +103,7 @@ void server_accept_cb(struct event_loop *loop, int fd, int mask, void *evdata)
             int remote_fd = accept(fd, NULL, NULL);
             if (remote_fd < 0) {
                 if (errno != EWOULDBLOCK) {
-                    LOG_WARN("accept() failed\n");
+                    LOG_WARN("accept() failed: %s", strerror(errno));
                     break;
                 }
                 continue;
@@ -144,7 +111,7 @@ void server_accept_cb(struct event_loop *loop, int fd, int mask, void *evdata)
             set_nonblocking(remote_fd);
             set_sock_option(remote_fd);
 
-            LOG_INFO("New incoming connection - %d\n", remote_fd);
+            LOG_INFO("new remote %d comming connection", remote_fd);
             create_event(loop, remote_fd, EV_RDABLE, &server_remote_reply_cb, NULL);
             break;
         }
@@ -156,10 +123,10 @@ void server_remote_reply_cb(struct event_loop *loop, int fd, int mask, void *evd
     int len = 0, remote_fd = fd;
     char buffer[BUFSIZE];
 
-    LOG_INFO("server and remote %d talk....\n", remote_fd);
+    /* 此处 while 是比较"脏"的用法 */
     while (1) {
         int rc = recv(remote_fd, buffer, BUFSIZE, 0);
-        printf("server and remote %d talk recv rc %d\n", remote_fd, rc);
+
         if (rc < 0) {
             if (errno != EAGAIN) {
                 LOG_WARN("recv() failed: %s", strerror(errno));
@@ -168,47 +135,49 @@ void server_remote_reply_cb(struct event_loop *loop, int fd, int mask, void *evd
             return;
         }
         if (rc == 0) {
-            LOG_WARN("remote %d Connection closed\n", remote_fd);
+            LOG_WARN("remote %d connection closed\n", remote_fd);
             break;
         }
 
-        //len += rc;
-        printf("server and remote %d talk recv len %d\n", remote_fd, rc);
-        if (buffer[0] != 0x05) {
-            LOG_WARN("Not socks5 requ.......");
-            break;
-        }   
-        LOG_WARN("server and remote %d talk....recv:", remote_fd);
-        
-        int client_fd = socks5_connect_client(buffer, rc, &len);
-        if (client_fd < 0) {
-            LOG_WARN("can't connect remote");
-            break;
-        }
-        printf("client_fd = %d\n", client_fd);
-        set_nonblocking(client_fd);
-        
-        context *c = malloc(sizeof(*c));
-        if (c == NULL) {
-            LOG_ERROR("Malloc Error");
-        }
-        malloc_count++;
-        c->client_fd = client_fd;
-        c->remote_fd = remote_fd;
-        LOG_WARN("remote %d and client %d add.............", remote_fd, client_fd);
-        c->sendlen = c->recvlen = 0;
-        c->loop = loop;
+        if (rc > 0) {
+            //decrypt(buffer, rc);
+            LOG_INFO("server and remote %d talk recv len %d", remote_fd, rc);
+            if (buffer[0] != 0x05) {
+                LOG_WARN("remote %d not socks5 request", remote_fd);
+                break;
+            }
 
-        delete_event(loop, remote_fd, EV_RDABLE);
-        if (rc > len) {
-            memcpy(c->crecv, buffer+len, rc-len);
-            c->recvlen = rc - len;
-            create_event(loop, c->client_fd, EV_WRABLE, &client_write_cb, c);
-        } else {
-            create_event(loop, remote_fd, EV_RDABLE, &remote_read_cb, c);    
-        }
-        memset(buffer, 0, BUFSIZE);
-        return;      
+            int client_fd = socks5_connect_client(buffer, rc, &len);
+            if (client_fd < 0) {
+                break;
+            }
+            set_nonblocking(client_fd);
+            
+            context *c = malloc(sizeof(*c));
+            if (c == NULL) {
+                LOG_ERROR("malloc new context errno");
+                break;
+            }
+
+            c->client_fd = client_fd;
+            c->remote_fd = remote_fd;
+            c->sendlen = c->recvlen = 0;
+            c->snow = c->rnow = 0;
+            c->loop = loop;
+
+            delete_event(loop, remote_fd, EV_RDABLE);
+            
+            /* buffer 中可能含有其它需要发送到 client 的数据 */
+            if (rc > len) {
+                memcpy(c->crecv, buffer+len, rc-len);
+                c->recvlen = rc - len;
+                create_event(loop, c->client_fd, EV_WRABLE, &client_writable_cb, c);
+            } else {
+                create_event(loop, remote_fd, EV_RDABLE, &remote_readable_cb, c);    
+            }
+            memset(buffer, 0, BUFSIZE);
+            return;       
+        }       
     }
 
     delete_event(loop, remote_fd, EV_WRABLE);
@@ -217,7 +186,8 @@ void server_remote_reply_cb(struct event_loop *loop, int fd, int mask, void *evd
     memset(buffer, 0, BUFSIZE);
 }
 
-void remote_write_cb(struct event_loop *loop, int fd, int mask, void *evdata)
+
+void remote_writable_cb(struct event_loop *loop, int fd, int mask, void *evdata)
 {
     context *c = (context *)evdata;
     
@@ -225,8 +195,9 @@ void remote_write_cb(struct event_loop *loop, int fd, int mask, void *evdata)
         close_and_free_remote(c);
         return;
     }
+
     while (1) {
-        int rc = send(fd, c->csend, c->sendlen, 0);
+        int rc = send(fd, c->csend + c->snow, c->sendlen, 0);
         if (rc < 0) {
             if (errno != EAGAIN) {
                 LOG_WARN("send() failed: %s", strerror(errno));
@@ -239,22 +210,26 @@ void remote_write_cb(struct event_loop *loop, int fd, int mask, void *evdata)
         if (rc >= 0) {
             c->sendlen -= rc;
             if (c->sendlen <= 0) {
+
+                c->snow = 0;
                 delete_event(loop, fd, EV_WRABLE);
+                
+                /* 如果 client 端已经关闭，则此次请求结束 */
                 if (c->client_fd == 0) {
                     close_and_free_remote(c);
                 } else {
-                    create_event(loop, fd, EV_RDABLE, &remote_read_cb, c);
-                    create_event(loop, c->client_fd, EV_RDABLE, &client_read_cb, c);      
+                    create_event(loop, fd, EV_RDABLE, &remote_readable_cb, c);
+                    create_event(loop, c->client_fd, EV_RDABLE, &client_readable_cb, c);      
                 }
-                break;   
+                break;  
+            } else {
+                c->snow += rc;
             }
-            continue;
-        }
-        
+        }  
     }  
 }
 
-void remote_read_cb(struct event_loop *loop, int fd, int mask, void *evdata)
+void remote_readable_cb(struct event_loop *loop, int fd, int mask, void *evdata)
 {
     context *c = (context *)evdata;
 
@@ -278,29 +253,26 @@ void remote_read_cb(struct event_loop *loop, int fd, int mask, void *evdata)
         }
 
         c->recvlen += rc;
-        if (c->recvlen > BUFSIZE - 5) {
-            delete_event(loop, fd, EV_RDABLE);
-            break;     
-        }  
+        //decrypt(c->crecv, c->recvlen);
+        
+        delete_event(loop, fd, EV_RDABLE);
+        break;     
     }
-    create_event(loop, c->client_fd, EV_WRABLE, &client_write_cb, c);
+    create_event(loop, c->client_fd, EV_WRABLE, &client_writable_cb, c);
 }
 
-void signal_handle()
-{
-    printf("malloc_count %d\n", malloc_count);
-    exit(0);
-}
 
 int main (int argc, char *argv[])
 {
+    //unsigned char* key = strdup("2343b3d537a54571f6060f40e14a4d9f");
+    //get_table(key);
     event_loop *loop;
     loop = create_event_loop(100);
     if (loop == NULL) {
         LOG_ERROR("Create Event Loop Error!");
     }
     
-    int listen_sd = create_and_bind("0.0.0.0", "8000");
+    int listen_sd = create_and_bind("0.0.0.0", "8888");
     if (listen_sd < 0)  {
        LOG_ERROR("socket() failed");
     }
@@ -310,7 +282,6 @@ int main (int argc, char *argv[])
     }
 
     create_event(loop, listen_sd, EV_RDABLE, &server_accept_cb, NULL);
-    signal(SIGINT, signal_handle);
     start_event_loop(loop);
     delete_event_loop(loop);
     
