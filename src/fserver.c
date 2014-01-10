c7391d439efabb8263d40e0f9e58510868523a14
Serho Liu <serholiu@gmail.com>
2014-01-10 16:45:08 +0800

    some done

--- src/fserver.c
+++ src/fserver.c
@@ -12,40 +12,82 @@
 
 #define HAND_DATA_SIZE 1024
 
-
 void client_handshake_cb(struct event_loop *loop, int fd, int mask, void *evdata)
 {
-    int client_fd = fd;
-    fakio_context_t *fctx = evdata;
+    int r, need, client_fd = fd;
+    context *c = evdata;
 
     while (1) {
-        int rc = recv(client_fd, fctx->hand+fctx->length, HAND_DATA_SIZE-fctx->length, 0);
+        need = HAND_DATA_SIZE - FBUF_DATA_LEN(c->req);
+        int rc = recv(client_fd, FBUF_WRITE_AT(c->req), need, 0);
 
         if (rc < 0) {
             if (errno == EAGAIN) {
                 return;    
             }
-            break;
+            goto done;
         }
         if (rc == 0) {
-            LOG_DEBUG("remote %d connection closed\n", client_fd);
-            break;
+            LOG_DEBUG("client %d connection closed", client_fd);
+            goto done;
         }
 
         if (rc > 0) {
-            fctx->length += rc;
-            if (fctx->length < HAND_DATA_SIZE) {
+            FBUF_COMMIT_WRITE(c->req, rc);
+            if (FBUF_DATA_LEN(c->req) < HAND_DATA_SIZE) {
                 continue;
             }
-
-            // TODO 解析
-
+            break;
         }
     }
 
+    // 用户认证
+    request_t req;
+    fakio_request_resolve(FBUF_DATA_AT(c->req), HAND_DATA_SIZE,
+                          &req, FNET_RESOLVE_USER);
+    aes_init(cfg.key, req.IV, c->e_ctx, c->d_ctx);
+
+    uint8_t buffer[HAND_DATA_SIZE];
+    int len = aes_decrypt(c->d_ctx, FBUF_DATA_AT(c->req), 
+                          HAND_DATA_SIZE-req.rlen, buffer+req.rlen);
+    r = fakio_request_resolve(buffer+req.rlen, len, &req, FNET_RESOLVE_NET);
+    if (r != 1) {
+        LOG_WARN("socks5 request resolve error");
+        goto done;
+    }
+    int remote_fd = fnet_create_and_connect(req.addr, req.port, FNET_CONNECT_NONBLOCK);
+    if (remote_fd < 0) {
+        goto done;
+    }
+
+    if (set_socket_option(remote_fd) < 0) {
+        LOG_WARN("set socket option error");
+    }
+    
+    LOG_DEBUG("client %d remote %d at %p", client_fd, remote_fd, c);
+    c->client_fd = client_fd;
+    c->remote_fd = remote_fd;
+    c->loop = loop;
+
+    random_key(buffer, 48);
+    memcpy(c->key, buffer+16, 32);
+    aes_init(c->key, buffer, c->e_ctx, c->d_ctx);
+    aes_encrypt(c->e_ctx, buffer+16, 32, buffer+48);
+    memcpy(buffer+16, buffer+48, 32);
+
+    //TODO:
+    send(client_fd, buffer, 48, 0);
+
+    delete_event(loop, client_fd, EV_RDABLE);
+    create_event(loop, client_fd, EV_RDABLE, &client_readable_cb, c);    
+    memset(buffer, 0, HAND_DATA_SIZE);
+    return;
+
+done:
     delete_event(loop, client_fd, EV_WRABLE);
     delete_event(loop, client_fd, EV_RDABLE);
     close(client_fd);
+    //TODO: 释放 context
 }
 
 
@@ -54,6 +96,7 @@ int main (int argc, char *argv[])
     if (argc != 2) {
         LOG_ERROR("Usage: %s --config_path\n", argv[0]);
     }
+    //config cfg;
     load_config_file(&cfg, argv[1]);
 
     //signal(SIGPIPE, SIG_IGN);
