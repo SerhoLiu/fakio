d762691743e011b08e59fe98f1f06c10132aab4a
Serho Liu <serholiu@gmail.com>
2014-01-15 14:51:21 +0800

    添加多用户

---
+++ src/fuser.c
@@ -0,0 +1,52 @@
+#include "fuser.h"
+#include <stdlib.h>
+#include "base/sha2.h"
+
+
+static int username_compare(const void *desc, int dlen,
+                            const void *src, int slen)
+{
+    if (dlen != slen) {
+        return dlen - slen;
+    }
+
+    return memcmp(desc, src, slen);
+}
+
+
+hashmap *fuser_userdict_create(unsigned long size)
+{
+    return hashmap_new(size, &username_compare);
+}
+
+
+int fuser_add_user(hashmap *users, const char *name, const char *password)
+{
+    if (users == NULL || name == NULL || password == NULL) return 0;
+
+    size_t nlen = strlen(name);
+    size_t plen = strlen(password);
+
+    if (nlen == 0 || plen == 0) return 0;
+
+    fuser_t *user = malloc(sizeof(*user));
+    if (user == NULL) return 0;
+
+    int i;
+    for (i = 0; i < nlen; i++) {
+        user->username[i] = name[i];
+    }
+    user->name_len = nlen;
+
+    sha2((uint8_t *)password, plen, user->key, 0);
+
+    return hashmap_put(users, user->username, nlen, user);
+}
+
+
+fuser_t *fuser_find_user(hashmap *users, uint8_t* name, int nlen)
+{
+    if (users == NULL || name == NULL || nlen == 0) return NULL;
+
+    return hashmap_get(users, name, nlen);
+}
--- src/fuser.h
+++ src/fuser.h
@@ -2,11 +2,17 @@
 #define _FAKIO_USER_H_
 
 #include "fakio.h"
+#include "base/hashmap.h"
 
 struct fuser {
     uint8_t username[MAX_USERNAME];
     int name_len;
-    uint8_t key[16];
+    uint8_t key[32];
 };
+
+
+hashmap *fuser_userdict_create(unsigned long size);
+int fuser_add_user(hashmap *users, const char *name, const char *password);
+fuser_t *fuser_find_user(hashmap *users, uint8_t* name, int nlen);
 
 #endif
