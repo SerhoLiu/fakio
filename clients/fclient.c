89eca60147c46b6a20be4355bfee145b053d087d
Serho Liu <serholiu@gmail.com>
2014-01-17 16:09:02 +0800

    log fix

--- clients/fclient.c
+++ clients/fclient.c
@@ -42,7 +42,7 @@ static void server_accept_cb(struct event_loop *loop, int fd, int mask, void *ev
         int client_fd = accept(fd, NULL, NULL);
         if (client_fd < 0) {
             if (errno != EWOULDBLOCK) {
-                LOG_WARN("accept() failed\n");
+                fakio_log(LOG_WARNING, "accept() failed\n");
                 break;
             }
             continue;
@@ -50,7 +50,7 @@ static void server_accept_cb(struct event_loop *loop, int fd, int mask, void *ev
         set_nonblocking(client_fd);
         set_socket_option(client_fd);
 
-        LOG_DEBUG("new client %d comming connection", client_fd);
+        LOG_FOR_DEBUG("new client %d comming connection", client_fd);
         create_event(loop, client_fd, EV_RDABLE, &socks5_handshake1_cb, NULL);
         break;
     }
@@ -68,11 +68,11 @@ void socks5_handshake1_cb(struct event_loop *loop, int fd, int mask, void *evdat
             if (errno == EAGAIN) {
                 return;
             }
-            LOG_DEBUG("recv() failed: %s", strerror(errno));
+            LOG_FOR_DEBUG("recv() failed: %s", strerror(errno));
             break;
         }
         if (rc == 0) {
-            LOG_DEBUG("client %d connection closed\n", client_fd);
+            LOG_FOR_DEBUG("client %d connection closed\n", client_fd);
             break;
         }
 
@@ -90,7 +90,7 @@ void socks5_handshake1_cb(struct event_loop *loop, int fd, int mask, void *evdat
             return;
 
         } else {
-            LOG_WARN("Client request not socks5!");
+            fakio_log(LOG_WARNING, "Client request not socks5!");
             break;
         }
     }
@@ -114,11 +114,11 @@ void socks5_handshake2_cb(struct event_loop *loop, int fd, int mask, void *evdat
             if (errno == EAGAIN) {
                 return;
             }
-            LOG_DEBUG("recv() failed: %s", strerror(errno));
+            LOG_FOR_DEBUG("recv() failed: %s", strerror(errno));
             break;
         }
         if (rc == 0) {
-            LOG_DEBUG("client %d connection closed\n", client_fd);
+            LOG_FOR_DEBUG("client %d connection closed\n", client_fd);
             break;
         }
 
@@ -127,7 +127,7 @@ void socks5_handshake2_cb(struct event_loop *loop, int fd, int mask, void *evdat
             int remote_fd = fnet_create_and_connect(client.shost,
                             client.sport, FNET_CONNECT_BLOCK);
             if (remote_fd < 0) {
-                LOG_WARN("Server don't onnection");
+                fakio_log(LOG_WARNING, "Server don't onnection");
                 break;
             }
             set_nonblocking(remote_fd);
@@ -146,11 +146,11 @@ void socks5_handshake2_cb(struct event_loop *loop, int fd, int mask, void *evdat
             
             context_t *c = context_pool_get(pool, MASK_CLIENT|MASK_REMOTE);
             if (c == NULL) {
-                LOG_WARN("Can't get context!");
+                fakio_log(LOG_WARNING, "Can't get context!");
                 close(remote_fd);
                 break;
             }
-            LOG_DEBUG("client %d remote %d at %p", client_fd, remote_fd, c);
+            LOG_FOR_DEBUG("client %d remote %d at %p", client_fd, remote_fd, c);
             c->client_fd = client_fd;
             c->remote_fd = remote_fd;
             c->loop = loop;
@@ -180,7 +180,7 @@ void socks5_handshake2_cb(struct event_loop *loop, int fd, int mask, void *evdat
             create_event(loop, c->remote_fd, EV_WRABLE, &server_handshake1_cb, c);
             return;
         } else {
-            LOG_WARN("Client request not socks5!");
+            fakio_log(LOG_WARNING, "Client request not socks5!");
             break;
         }
     }
@@ -201,7 +201,7 @@ void server_handshake1_cb(struct event_loop *loop, int fd, int mask, void *evdat
             if (errno == EAGAIN) {
                 return;
             }
-            LOG_DEBUG("send() to remote %d failed: %s", fd, strerror(errno));
+            LOG_FOR_DEBUG("send() to remote %d failed: %s", fd, strerror(errno));
             context_pool_release(c->pool, c, MASK_CLIENT|MASK_REMOTE);
             return;
         }
@@ -234,12 +234,12 @@ void server_handshake2_cb(struct event_loop *loop, int fd, int mask, void *evdat
             if (errno == EAGAIN) {
                 return;
             }
-            LOG_DEBUG("recv() from remote %d failed: %s", fd, strerror(errno));
+            LOG_FOR_DEBUG("recv() from remote %d failed: %s", fd, strerror(errno));
             context_pool_release(c->pool, c, MASK_CLIENT|MASK_REMOTE);
             return;
         }
         if (rc == 0) {
-            LOG_DEBUG("remote %d connection closed", fd);
+            LOG_FOR_DEBUG("remote %d connection closed", fd);
             context_pool_release(c->pool, c, MASK_CLIENT|MASK_REMOTE);
             return;
         }
@@ -275,12 +275,12 @@ static void remote_readable_cb(struct event_loop *loop, int fd, int mask, void *
             if (errno == EAGAIN) {
                 return;
             }
-            LOG_DEBUG("recv() from remote %d failed: %s", fd, strerror(errno));
+            LOG_FOR_DEBUG("recv() from remote %d failed: %s", fd, strerror(errno));
             context_pool_release(c->pool, c, MASK_CLIENT|MASK_REMOTE);
             return;
         }
         if (rc == 0) {
-            LOG_DEBUG("remote %d connection closed", fd);
+            LOG_FOR_DEBUG("remote %d connection closed", fd);
             context_pool_release(c->pool, c, MASK_CLIENT|MASK_REMOTE);
             return;
         }
@@ -307,7 +307,7 @@ static void remote_writable_cb(struct event_loop *loop, int fd, int mask, void *
             if (errno == EAGAIN) {
                 return;
             }
-            LOG_DEBUG("send() to remote %d failed: %s", fd, strerror(errno));
+            LOG_FOR_DEBUG("send() to remote %d failed: %s", fd, strerror(errno));
             context_pool_release(c->pool, c, MASK_CLIENT|MASK_REMOTE);
             return;
         }
@@ -340,7 +340,7 @@ static void client_writable_cb(struct event_loop *loop, int fd, int mask, void *
             if (errno == EAGAIN) {
                 return;
             }
-            LOG_DEBUG("send() failed to client %d: %s", fd, strerror(errno));
+            LOG_FOR_DEBUG("send() failed to client %d: %s", fd, strerror(errno));
             context_pool_release(c->pool, c, MASK_CLIENT|MASK_REMOTE);
             return;
         }
@@ -373,12 +373,12 @@ static void client_readable_cb(struct event_loop *loop, int fd, int mask, void *
         if (errno == EAGAIN) {
                 return;
         }
-        LOG_DEBUG("recv() failed form client %d: %s", fd, strerror(errno));
+        LOG_FOR_DEBUG("recv() failed form client %d: %s", fd, strerror(errno));
         context_pool_release(c->pool, c, MASK_CLIENT|MASK_REMOTE);
         return;
     }
     if (rc == 0) {
-        LOG_DEBUG("client %d Connection closed", fd);
+        LOG_FOR_DEBUG("client %d Connection closed", fd);
         context_pool_release(c->pool, c, MASK_REMOTE|MASK_CLIENT);
         return;
     }
@@ -395,7 +395,7 @@ static int config_handler(void* user, const char* section, const char* name,
 {
     fclient_t *client = user;
 
-    LOG_DEBUG("load conf: %s %s %s", section, name, value);
+    LOG_FOR_DEBUG("load conf: %s %s %s", section, name, value);
 
     if (strcmp("server", section) == 0) {
         if (strcmp("host", name) == 0) {
@@ -423,7 +423,8 @@ static int config_handler(void* user, const char* section, const char* name,
         if (strcmp("name", name) == 0) {
             size_t nlen = strlen(value);
             if (nlen > MAX_USERNAME) {
-                LOG_ERROR("User Name too long, must %d!", MAX_USERNAME);
+                fakio_log(LOG_ERROR, "User Name too long, must %d!", MAX_USERNAME);
+                exit(1);
             }
             int i;
             for (i = 0; i < nlen; i++) {
@@ -446,11 +447,13 @@ static void client_load_config_file(const char *filename, fclient_t *client)
 {
     FILE *f = fopen(filename, "rb");
     if (f == NULL) {
-        LOG_ERROR("Can't load config file: %s", filename);
+        fakio_log(LOG_ERROR, "Can't load config file: %s", filename);
+        exit(1);
     }
 
     if (ini_parse_file(f, &config_handler, client) < 0) {
-        LOG_ERROR("Can't load config file: %s", filename);
+        fakio_log(LOG_ERROR, "Can't load config file: %s", filename);
+        exit(1);
     }
 
     fclose(f);
@@ -460,7 +463,8 @@ static void client_load_config_file(const char *filename, fclient_t *client)
 int main (int argc, char *argv[])
 {
     if (argc != 2) {
-        LOG_ERROR("Usage: %s --config_path\n", argv[0]);
+        fakio_log(LOG_ERROR, "Usage: %s --config_path\n", argv[0]);
+        exit(1);
     }
 
     client_load_config_file(argv[1], &client);
@@ -468,28 +472,32 @@ int main (int argc, char *argv[])
     /* 初始化 Context */
     pool = context_pool_create(100);
     if (pool == NULL) {
-        LOG_ERROR("Start Error!");
+        fakio_log(LOG_ERROR, "Start Error!");
+        exit(1);
     }
     
     event_loop *loop;
     loop = create_event_loop(100);
     if (loop == NULL) {
-        LOG_ERROR("Create Event Loop Error!");
+        fakio_log(LOG_ERROR, "Create Event Loop Error!");
+        exit(1);
     }
     
     /* NULL is 0.0.0.0 */
     int listen_sd = fnet_create_and_bind(client.chost, client.cport);
     if (listen_sd < 0)  {
-       LOG_ERROR("socket() failed");
+        fakio_log(LOG_ERROR, "socket() failed");
+        exit(1);
     }
     set_nonblocking (listen_sd);
     if (listen(listen_sd, SOMAXCONN) == -1) {
-        LOG_ERROR("socket() failed");
+        fakio_log(LOG_ERROR, "socket() failed");
+        exit(1);
     }
 
     create_event(loop, listen_sd, EV_RDABLE, &server_accept_cb, NULL);
-    LOG_INFO("Fakio client start... binding in %s:%s", client.chost, client.cport);
-    LOG_INFO("Fakio client event loop start, use %s", get_event_api_name());
+    fakio_log(LOG_INFO, "Fakio client start... binding in %s:%s", client.chost, client.cport);
+    fakio_log(LOG_INFO, "Fakio client event loop start, use %s", get_event_api_name());
     start_event_loop(loop);
 
     //context_list_free(list);
