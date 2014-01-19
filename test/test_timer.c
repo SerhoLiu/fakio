33f672ccd3922c8d55d2851e9aef959d70ac740b
Serho Liu <serholiu@gmail.com>
2014-01-19 21:23:53 +0800

    add timer

--- test/test_timer.c
+++ test/test_timer.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <time.h>
+#include "../src/base/fevent.h"
 
-
-int main(int argc, char **argv)
+long print_callback(struct event_loop *loop, long long id, void *evdata)
 {
     struct timespec t;
     clock_gettime(CLOCK_MONOTONIC, &t);
     printf("%ld %ld\n", t.tv_sec, t.tv_nsec);
+    return 2000;
+}
+
+int main(int argc, char **argv)
+{
+    event_loop *loop = create_event_loop(100);
+    create_time_event(loop, 10000, &print_callback, NULL);
+
+    struct timespec t;
+    clock_gettime(CLOCK_MONOTONIC, &t);
+    printf("%ld %ld\n", t.tv_sec, t.tv_nsec);
+    start_event_loop(loop);
     return 0;
 }
