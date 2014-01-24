3ee1bbf7ada5d6cb5c090b65963e6b3b20b305ef
Serho Liu <serholiu@gmail.com>
2014-01-24 16:58:40 +0800

    update

--- test/fbench.go
+++ test/fbench.go
@@ -9,12 +9,15 @@ import (
 	"crypto/sha256"
 	"encoding/binary"
 	"errors"
+	"flag"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"net"
 	"net/http"
+	"net/url"
 	"strconv"
+	"strings"
+	//"sync"
+	"time"
 )
 
 const (
@@ -22,6 +25,14 @@ const (
 	TestPass   = "123456"
 	TestServer = "localhost:8888"
 )
+
+var N, C int
+
+//var T time.Duration
+var URL string
+
+var ErrHandToServer = errors.New("handshake to server error")
+var ErrHandFromServer = errors.New("handshake from server error")
 
 //crypto
 func stringToKey(str string) (key []byte) {
@@ -114,12 +125,12 @@ func FakioDial(addr, server string) (c *FakioConn, err error) {
 	// 22 = iv + username
 	enc.XORKeyStream(req[22:], req[22:])
 	if _, err := conn.Write(req); err != nil {
-		return nil, errors.New("hanshake to server error")
+		return nil, ErrHandToServer
 	}
 
 	hand := make([]byte, 64)
 	if _, err := conn.Read(hand); err != nil {
-		return nil, errors.New("hanshake from server error")
+		return nil, ErrHandFromServer
 	}
 
 	dec := cipher.NewCFBDecrypter(block, hand[0:16])
@@ -146,29 +157,122 @@ func (c *FakioConn) Write(b []byte) (n int, err error) {
 	return n, err
 }
 
-func main() {
+type Result struct {
+	ok  int64
+	err int64
+	t   time.Duration
+}
 
-	tr := &http.Transport{
-		Dial: func(_, _ string) (net.Conn, error) {
-			return FakioDial("localhost:8000", TestServer)
-		},
+func doGet(client *http.Client) (t time.Duration, err error) {
+	s := time.Now()
+	resq, err := client.Get(URL)
+	t = time.Now().Sub(s)
+
+	if err == nil {
+		resq.Body.Close()
+	}
+	return
+}
+
+func clients(r chan Result, client *http.Client) {
+	result := Result{0, 0, 0}
+
+	defer func() {
+		r <- result
+	}()
+
+	//var oks, errs int64
+
+	for i := 0; i < N; i++ {
+		t, err := doGet(client)
+		result.t += t
+		if err != nil {
+			fmt.Println(err)
+			result.err += 1
+		} else {
+			result.ok += 1
+		}
+	}
+}
+
+func run(client *http.Client) {
+	results := make([]chan Result, C)
+
+	var okReq, errReq int64
+	var allTime time.Duration
+
+	start := time.Now()
+	for i := 0; i < C; i++ {
+		results[i] = make(chan Result)
+		go clients(results[i], client)
 	}
 
-	client := &http.Client{Transport: tr}
+	for _, ch := range results {
+		req := <-ch
+		okReq += req.ok
+		errReq += req.err
+		allTime += req.t
+	}
 
-	resp, err := client.Get("http://localhost:8000/")
-	if err != nil {
-		fmt.Println(err)
+	total := time.Now().Sub(start)
+
+	fmt.Printf("\nSummary:\n")
+	fmt.Printf("        Total Request:\t%d total, %d susceed, %d failed.\n", N*C, okReq, errReq)
+	fmt.Printf("           Total time:\t%4.4f secs.\n", total.Seconds())
+	fmt.Printf("         Average time:\t%4.4f secs.\n", allTime.Seconds()/float64(C))
+	fmt.Printf("     Time per request:\t%4.4f secs.\n", allTime.Seconds()/float64(C))
+	fmt.Printf("  Requests per second:\t%4.4f\n", float64(N*C)/total.Seconds())
+}
+
+func main() {
+	var useProxy bool
+
+	flag.IntVar(&C, "c", 100, "The number of concurrent clients to run")
+	flag.IntVar(&N, "n", 100, "Total number of per client requests to make")
+	flag.BoolVar((*bool)(&useProxy), "f", false, "use fakio proxy")
+
+	flag.Parse()
+
+	if len(flag.Args()) != 1 {
+		fmt.Println("Usage: fbench -c clients -t times url")
 		return
 	}
 
-	buf, err := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(buf))
-
-	if err != io.EOF {
-		fmt.Printf("Read response error: %v\n", err)
-	} else {
-		err = nil
+	URL = flag.Arg(0)
+	if strings.HasPrefix(URL, "https://") {
+		fmt.Println("https not supported")
+		return
+	}
+	if !strings.HasPrefix(URL, "http://") {
+		URL = "http://" + URL
 	}
 
+	parsedURL, err := url.Parse(URL)
+	if err != nil {
+		fmt.Println("Error parsing url:", err)
+		return
+	}
+
+	var client *http.Client
+
+	if useProxy {
+		host, _, err := net.SplitHostPort(parsedURL.Host)
+		if err != nil {
+			host = net.JoinHostPort(parsedURL.Host, "80")
+		} else {
+			host = parsedURL.Host
+		}
+
+		tr := &http.Transport{
+			Dial: func(_, _ string) (net.Conn, error) {
+				return FakioDial(host, TestServer)
+			},
+		}
+
+		client = &http.Client{Transport: tr}
+	} else {
+		client = &http.Client{}
+	}
+
+	run(client)
 }
